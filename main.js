f69f688a89890bacdca37bb1bc3085344dcc5b4a
EssenceOfApple <57086013+EssenceOfApple@users.noreply.github.com>
2023-06-14 03:31:15 +0300

    CGW

--- main.js
+++ main.js
@@ -1,39 +1,42 @@
 'use strict';
 
-let gl;                         // The webgl context.
-let surface;                    // A surface model
-let shProgram;                  // A shader program
-let spaceball;                  // A SimpleRotator object that lets the user rotate the view by mouse.
+let gl;                         
+let surface;                    
+let shProgram;                  
+let spaceball;                  
 let height = 5;
 let p = 3;
 let length;
-let surfaceType;
 let inputData;
-let rotation = {x: 0, y: 0, z: 0};
+var playButton;
+let rotation = {x:0, y: 0, z:0};
 let N = 20;
-let lightPos = [0, 0, 0];
+let position = {x:1, y: 0, z:0};
+var audioContext;
+let sound;
+let radius = 0.5;
+let soundFileName = "sound.mp3";
+let source;
+let panner;
+let highpassFilter;
+let defaultFrequency;
 
 function deg2rad(angle) {
     return angle * Math.PI / 180;
 }
 
-function GetRadiansFromDegree(angle) {
-    return angle * Math.PI / 180;
-}
-
-// Constructor
 function Model(name) {
     this.name = name;
     this.iVertexBuffer = gl.createBuffer();
     this.iTextureCoordBuffer = gl.createBuffer();
     this.count = 0;
 
-    this.BufferData = function (vertices) {
+    this.BufferData = function(vertices) {
 
         gl.bindBuffer(gl.ARRAY_BUFFER, this.iVertexBuffer);
         gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STREAM_DRAW);
 
-        this.count = vertices.length / 3;
+        this.count = vertices.length/3;
     }
 
     this.TextureBufferData = function (textureCoords) {
@@ -55,8 +58,25 @@ function Model(name) {
             gl.drawArrays(gl.TRIANGLE_STRIP, i * N, N);
         }
     };
-}
 
+    this.DrawByLines = function () {
+        gl.bindBuffer(gl.ARRAY_BUFFER, this.iVertexBuffer);
+        gl.vertexAttribPointer(shProgram.iAttribVertex, 3, gl.FLOAT, false, 0, 0);
+        gl.enableVertexAttribArray(shProgram.iAttribVertex);
+
+        gl.vertexAttribPointer(shProgram.iNormal, 3, gl.FLOAT, true, 0, 0);
+        gl.enableVertexAttribArray(shProgram.iNormal);
+
+        if (inputData.surfaceType.checked) {
+            gl.drawArrays(gl.LINE_STRIP, 0, this.count);
+        } else {
+            const stepLength = this.count / length;
+            for (let step = 0; step < this.count; step += stepLength) {
+                gl.drawArrays(gl.LINE_STRIP, step, stepLength);
+            }
+        }
+    };
+}
 
 // Constructor
 function ShaderProgram(name, program) {
@@ -79,17 +99,18 @@ function ShaderProgram(name, program) {
     this.iShininess = -1;
     this.iLightPos = -1;
 
-    this.Use = function () {
+    this.Use = function() {
         gl.useProgram(this.prog);
     }
 }
-
-function UpdateInputData() {
+function UpdateInputData()
+{
     this.convergence = 2000.0;
     this.fov = 1.0;
-    this.eyeSeparation = 80.0;
-    ;this.near = 10;
+    this.eyeSeparation = 80.0;;
+    this.near = 10;
     this.surfaceType = true;
+    this.filter = true;
     this.lightPosition = -1;
     this.ratio = 1;
     this.far = 2000.0;
@@ -101,17 +122,20 @@ function UpdateInputData() {
     this.left;
     this.right;
 
-    this.UpdateData = function () {
+    this.UpdateData = function()
+    {
         this.convergence = document.getElementById("convergence").value;
         this.eyeSeparation = document.getElementById("eyeSeparation").value;
         this.fov = document.getElementById("FOV").value;
         this.near = document.getElementById("nearDistance").value - 0.0;
         this.surfaceType = document.getElementById('SurfaceType');
+        this.filter = document.getElementById('filter');
         this.lightPosition = document.getElementById('light');
         this.UpdateDataForSides();
     }
 
-    this.UpdateDataForSides = function () {
+    this.UpdateDataForSides = function()
+    {
         this.a = this.ratio * Math.tan(this.fov / 2.0) * this.convergence;
 
         this.b = this.a - this.eyeSeparation / 2;
@@ -121,21 +145,19 @@ function UpdateInputData() {
         this.bottom = -this.top;
     }
 
-    this.UpdateSidesForLeftProjection = function () {
+    this.UpdateSidesForLeftProjection = function()
+    {
         this.left = -this.b * this.near / this.convergence;
         this.right = this.c * this.near / this.convergence;
     }
 
-    this.UpdateSidesForRightProjection = function () {
+    this.UpdateSidesForRightProjection = function()
+    {
         this.left = -this.c * this.near / this.convergence;
         this.right = this.b * this.near / this.convergence;
     }
 }
 
-/* Draws a colored cube, along with a set of coordinate axes.
- * (Note that the use of the above drawPrimitive function is not an efficient
- * way to draw with WebGL.  Here, the geometry is so simple that it doesn't matter.)
- */
 function draw() {
     gl.clearColor(0, 0, 0, 1);
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
@@ -148,10 +170,7 @@ function draw() {
     inputData.UpdateSidesForRightProjection();
     let rightProjection = m4.orthographic(inputData.left, inputData.right, inputData.bottom, inputData.top, inputData.near, inputData.far);
 
-    let modelView = m4.identity();
-    modelView = m4.xRotate(modelView, rotation.x);
-    modelView = m4.yRotate(modelView, rotation.y);
-    modelView = m4.zRotate(modelView, rotation.z);
+    let modelView = spaceball.getViewMatrix();
 
     let rotateToPointZero = m4.axisRotation([0.707, 0.707, 0], 0);
     let translateToPointZero = m4.translation(0, 0, 0);
@@ -189,6 +208,7 @@ function draw() {
     gl.uniformMatrix4fv(shProgram.iProjectionMatrix, false, leftProjection);
     gl.colorMask(true, false, false, false);
     surface.Draw();
+    sound.DrawByLines();
 
     gl.clear(gl.DEPTH_BUFFER_BIT);
 
@@ -196,13 +216,9 @@ function draw() {
     gl.uniformMatrix4fv(shProgram.iProjectionMatrix, false, rightProjection);
     gl.colorMask(false, true, true, false);
     surface.Draw();
+    sound.DrawByLines();
 
     gl.colorMask(true, true, true, true);
-}
-
-
-function GetCurrentZPosition(h) {
-    return Math.pow(Math.abs(h) - height, 2) / (2 * p);
 }
 
 function CreateSurfaceData() {
@@ -246,112 +262,201 @@ function KleinBottle(u, v) {
     return { x: x * multiplier, y: y * multiplier, z: z * multiplier }
 }
 
+function CreateSoundData()
+{
+    let vertexList = [];
+
+    const stepU = inputData.surfaceType.checked ? 5 : 10;
+
+    for (let u = 0; u <= 360; u += stepU)
+    {
+        for(let v = 0; v <= 360; v += stepU)
+        {
+            let alpha = deg2rad(u);
+            let beta = deg2rad(v);
+
+            vertexList.push(rotation.x +  (radius *  Math.cos(alpha) * Math.sin(beta)),rotation.y +
+                (radius *  Math.sin(alpha) * Math.sin(beta)),rotation.z + (radius *  Math.cos(beta)));
+        }
+    }
+
+    length = 360;
+    return vertexList;
+}
 
 /* Initialize the WebGL context. Called from init() */
 function initGL() {
-    let prog = createProgram(gl, vertexShaderSource, fragmentShaderSource);
+    let prog = createProgram( gl, vertexShaderSource, fragmentShaderSource );
 
     shProgram = new ShaderProgram('Basic', prog);
     shProgram.Use();
 
-    shProgram.iAttribVertex = gl.getAttribLocation(prog, "vertex");
-    shProgram.iAttribTexture = gl.getAttribLocation(prog, 'texture');
+    shProgram.iAttribVertex              = gl.getAttribLocation(prog, "vertex");
+    shProgram.iAttribTexture             = gl.getAttribLocation(prog, 'texture');
     shProgram.iModelViewProjectionMatrix = gl.getUniformLocation(prog, "ModelViewProjectionMatrix");
-    shProgram.iModelViewMatrix = gl.getUniformLocation(prog, "ModelViewMatrix");
-    shProgram.iProjectionMatrix = gl.getUniformLocation(prog, "ProjectionMatrix");
-    shProgram.iColor = gl.getUniformLocation(prog, "color");
-    shProgram.iNormal = gl.getAttribLocation(prog, 'normal');
-    shProgram.iNormalMatrix = gl.getUniformLocation(prog, 'normalMat');
-    shProgram.iAmbientColor = gl.getUniformLocation(prog, 'ambientColor');
-    shProgram.iDiffuseColor = gl.getUniformLocation(prog, 'diffuseColor');
-    shProgram.iSpecularColor = gl.getUniformLocation(prog, 'specularColor');
-    shProgram.iShininess = gl.getUniformLocation(prog, 'shininess');
-    shProgram.iLightPos = gl.getUniformLocation(prog, 'lightPosition');
-    shProgram.iSpecularCoefficient = gl.getUniformLocation(prog, 'specularCoefficient');
-    shProgram.iAmbientCoefficient = gl.getUniformLocation(prog, 'ambientCoefficient');
-    shProgram.iDiffuseCoefficient = gl.getUniformLocation(prog, 'diffuseCoefficient');
-    shProgram.iTMU = gl.getUniformLocation(prog, 'tmu');
+    shProgram.iModelViewMatrix           = gl.getUniformLocation(prog, "ModelViewMatrix");
+    shProgram.iProjectionMatrix            = gl.getUniformLocation(prog, "ProjectionMatrix");
+    shProgram.iColor                     = gl.getUniformLocation(prog, "color");
+    shProgram.iNormal                    = gl.getAttribLocation(prog, 'normal');
+    shProgram.iNormalMatrix              = gl.getUniformLocation(prog, 'normalMat');
+    shProgram.iAmbientColor              = gl.getUniformLocation(prog, 'ambientColor');
+    shProgram.iDiffuseColor              = gl.getUniformLocation(prog, 'diffuseColor');
+    shProgram.iSpecularColor             = gl.getUniformLocation(prog, 'specularColor');
+    shProgram.iShininess                 = gl.getUniformLocation(prog, 'shininess');
+    shProgram.iLightPos                  = gl.getUniformLocation(prog, 'lightPosition');
+    shProgram.iSpecularCoefficient       = gl.getUniformLocation(prog, 'specularCoefficient');
+    shProgram.iAmbientCoefficient        = gl.getUniformLocation(prog, 'ambientCoefficient');
+    shProgram.iDiffuseCoefficient        = gl.getUniformLocation(prog, 'diffuseCoefficient');
+    shProgram.iTMU                       = gl.getUniformLocation(prog, 'tmu');
+
+
 
 
     inputData = new UpdateInputData();
     inputData.UpdateData();
     surface = new Model('Surface');
     surface.BufferData(CreateSurfaceData());
+    sound = new Model('Sound');
+    sound.BufferData(CreateSoundData());
     gl.enable(gl.DEPTH_TEST);
 }
 
-/* Creates a program for use in the WebGL context gl, and returns the
- * identifier for that program.  If an error occurs while compiling or
- * linking the program, an exception of type Error is thrown.  The error
- * string contains the compilation or linking error.  If no error occurs,
- * the program identifier is the return value of the function.
- * The second and third parameters are strings that contain the
- * source code for the vertex shader and for the fragment shader.
- */
+function Sound()
+{
+    audioContext = new window.AudioContext();
+    CreateFilter();
+    CreatePanner();
+    const request = new XMLHttpRequest();
+    source = audioContext.createBufferSource();
+    request.open("GET", soundFileName, true);
+    request.responseType = "arraybuffer";
+
+    request.onload = () => {
+        const audioData = request.response;
+
+        audioContext.decodeAudioData(audioData, (buffer) => {
+
+                source.buffer = buffer;
+                source.connect(highpassFilter);
+                highpassFilter.connect(panner);
+                panner.connect(audioContext.destination);
+                source.loop = true;
+            }, (err) => {alert(err)}
+        );
+    };
+
+    request.send();
+    source.start(0);
+    playButton.disabled = true;
+    playButton.style.display = 'none';
+}
+
+function CreateFilter()
+{
+    highpassFilter = audioContext.createBiquadFilter();
+    highpassFilter.type = "highpass";
+    defaultFrequency =  highpassFilter.frequency.value;
+    highpassFilter.frequency.value = 1000;
+}
+
+function CreatePanner()
+{
+    panner = audioContext.createPanner();
+    panner.panningModel = "HRTF";
+    panner.distanceModel = "inverse";
+    panner.refDistance = 1;
+    panner.maxDistance = 1000;
+    panner.rolloffFactor = 1;
+    panner.coneInnerAngle = 360;
+    panner.coneOuterAngle = 0;
+    panner.coneOuterGain = 0;
+}
+
 function createProgram(gl, vShader, fShader) {
-    let vsh = gl.createShader(gl.VERTEX_SHADER);
-    gl.shaderSource(vsh, vShader);
+    let vsh = gl.createShader( gl.VERTEX_SHADER );
+    gl.shaderSource(vsh,vShader);
     gl.compileShader(vsh);
-    if (!gl.getShaderParameter(vsh, gl.COMPILE_STATUS)) {
+    if ( ! gl.getShaderParameter(vsh, gl.COMPILE_STATUS) ) {
         throw new Error("Error in vertex shader:  " + gl.getShaderInfoLog(vsh));
     }
-    let fsh = gl.createShader(gl.FRAGMENT_SHADER);
+    let fsh = gl.createShader( gl.FRAGMENT_SHADER );
     gl.shaderSource(fsh, fShader);
     gl.compileShader(fsh);
-    if (!gl.getShaderParameter(fsh, gl.COMPILE_STATUS)) {
+    if ( ! gl.getShaderParameter(fsh, gl.COMPILE_STATUS) ) {
         throw new Error("Error in fragment shader:  " + gl.getShaderInfoLog(fsh));
     }
     let prog = gl.createProgram();
-    gl.attachShader(prog, vsh);
+    gl.attachShader(prog,vsh);
     gl.attachShader(prog, fsh);
     gl.linkProgram(prog);
-    if (!gl.getProgramParameter(prog, gl.LINK_STATUS)) {
+    if ( ! gl.getProgramParameter( prog, gl.LINK_STATUS) ) {
         throw new Error("Link error in program:  " + gl.getProgramInfoLog(prog));
     }
     return prog;
 }
 
 
-/**
- * initialization function that will be called when the page has loaded
- */
 function init() {
 
     let canvas;
     try {
         canvas = document.getElementById("webglcanvas");
         gl = canvas.getContext("webgl");
-        if (!gl) {
+        if ( ! gl ) {
             throw "Browser does not support WebGL";
         }
-    } catch (e) {
-        document.getElementById("canvas-holder").innerHTML = "<p>Sorry, could not get a WebGL graphics context.</p>";
+    }
+    catch (e) {
+        document.getElementById("canvas-holder").innerHTML =
+            "<p>Sorry, could not get a WebGL graphics context.</p>";
         return;
     }
     try {
-        initGL();  // initialize the WebGL graphics context
-    } catch (e) {
-        document.getElementById("canvas-holder").innerHTML = "<p>Sorry, could not initialize the WebGL graphics context: " + e + "</p>";
+        initGL();  
+    }
+    catch (e) {
+        document.getElementById("canvas-holder").innerHTML =
+            "<p>Sorry, could not initialize the WebGL graphics context: " + e + "</p>";
         return;
     }
 
+
     window.addEventListener('devicemotion', (event) => {
-        rotation.x += GetRadiansFromDegree(event.acceleration.x);
-        rotation.y += GetRadiansFromDegree(event.acceleration.y);
-        rotation.z += GetRadiansFromDegree(event.acceleration.z);
+        if(panner)
+        {
+            position.x += deg2rad(event.acceleration.x);
+            position.y += deg2rad(event.acceleration.y);
+            position.z += deg2rad(event.acceleration.z);
+
+            rotation.x = 2 * Math.cos(position.y)*Math.cos(position.x);
+            rotation.y = 2 * Math.sin(position.y);
+            rotation.z = 2 * Math.cos(position.y)*Math.sin(position.z);
+
+            panner.setPosition(rotation.x, rotation.y, rotation.z);
+            panner.setOrientation(0,0,0);
+            Redraw();
+        }
     })
 
+    playButton = document.getElementById("play-button");
+    playButton.addEventListener('click', function(){
+
+        Sound();
+    });
     spaceball = new TrackballRotator(canvas, draw, 0);
     Update();
 }
 
-
-function Update() {
+function Update()
+{
     draw()
     window.requestAnimationFrame(Update);
 }
 
 function Redraw() {
+    highpassFilter.frequency.value = inputData.filter.checked ? 1000 : defaultFrequency;
     surface.BufferData(CreateSurfaceData());
+    sound.BufferData(CreateSoundData());
     draw();
 }
+
